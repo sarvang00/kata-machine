0d9248166f2fc1a605801a555d06fd635f3b6827
Sarvang Dave <sarvang00@gmail.com>
2024-02-27 00:32:46 -0400

    Solution: 2 Crystal Balls [O(root n)]

---
+++ src/day2/TwoCrystalBalls.ts
@@ -0,0 +1,20 @@
+export default function two_crystal_balls(breaks: boolean[]): number {
+    let jumpAmt = Math.floor(Math.sqrt(breaks.length));
+    let i=jumpAmt
+
+    for(; i<breaks.length; i+=jumpAmt) {
+        if (breaks[i]) {
+            break;
+        }
+    }
+    
+    i-=jumpAmt
+
+    for(let j=0; j< jumpAmt && i<breaks.length; j++, i++) {
+        if(breaks[i]) {
+            return i;
+        }
+    }
+
+    return -1;
+}
